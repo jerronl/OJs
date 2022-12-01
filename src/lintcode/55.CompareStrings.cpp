a338a9965b5dbda24cf359c1f8b885b6bef2c31c
jerronl <jerron@gmail.com>
2022-11-30 22:28:18 -0500

    Revert "bug fix or 'Z'"
    This reverts commit edfe600f47c7109508ee13978cb4f3c90ef9bda7.

--- src/lintcode/55.CompareStrings.cpp
+++ src/lintcode/55.CompareStrings.cpp
@@ -1,46 +1,37 @@
 /*
-* 55.Compare-Strings.cpp
-* Author:jerron
-* https://lintcode.com/problem/55
-* Compare two strings A and B, determine whether A contains all characters in B
-*/
-#include "util.h"
+ * 55. Compare Strings.cpp
+ *
+ *      Author: jerron
+ *      https://www.lintcode.com/problem/55
+ *      Compare two strings A and B, determine whether A contains all of the characters in B.
+ */
+#include "../utils/utils.h"
+
 class Solution {
 public:
-  /**
-   * @param A: A string
-   * @param B: A string
-   * @return: if string A contains all of the characters in B return true else
-   * return false
-   */
-  bool compareStrings(const string &A, const string &B) {
-    vector<int> S('Z'-'A'+1, 0);
-    for (auto c : A)
-      ++S[c - 'A'];
-    for (auto c : B)
-      if (--S[c - 'A'] < 0)
-        return false;
-    return true;
-  }
+    /**
+     * @param A: A string
+     * @param B: A string
+     * @return: if string A contains all of the characters in B return true else return false
+     */
+    bool compareStrings(const string &A,const string &B) {
+        vector<int> S('Z'-'A');
+        for(auto c:A)
+            ++S[c-'A'];
+        for(auto c:B)
+            if(--S[c-'A']<0)
+                return false;
+        return true;            
+    }
 };
 
-int main() {
-  Solution s;
-  vector<pair<vector<string>, bool>> tests = {
-      {{"abab", "aa"}, 1},
-      {{"A", "B"}, 0},
-      {{"AZ", "AZ"}, 1},
-      {{"AZ", "BZ"}, 0},
-      {{"PQRTZ","RSTUV"},0}
-
-  };
-  for (auto test : tests) {
-    auto tmp = test.first;
-    if (s.compareStrings(tmp[0], tmp[1]) != test.second) {
-      cout << "FAILED!" << endl;
-      return 0;
-    }
-  }
-  cout << "SUCCEED!";
-  return 0;
+int main(){
+    Solution s;
+    if( s.compareStrings("ABCD","ACD") )
+        cout<<"SUCCEED!";
+    else
+        cout<<"FAILED!";
+    return 0;
 }
+
+
